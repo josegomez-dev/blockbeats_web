76b0a1cca673e3cf9be08e250522a1336b84f7fb
Jose Alejandro Gomez Castro <josegomez.dev@gmail.com>
2025-05-30 15:05:44 -0600

    asd

--- vainilla-JS-reactive-components-main/components/loading-screen/script.js
+++
@@ -1,7 +0,0 @@
-const loadingScreen = document.getElementById('loading-screen');
-
-// Simulate loading time
-  setTimeout(() => {
-    // Hide loading screen after content is loaded
-    loadingScreen.style.display = 'none';
-}, 1000); // Adjust loading time as needed
